5e3315ac8b0e9717560000230210bf36eb585454
ANKUSH ARORA <127427325+Ankush-Arora-0@users.noreply.github.com>
2024-11-05 22:58:15 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -138,7 +138,7 @@ export const Home = () => {
   return (
     <div className='home-page'>
       <h2>Resume Builder</h2>
-      <p>By Ankush Arora</p>
+      <p>By Aisha Kumari</p>
       <div className="main-box">
       <div className="left-box">
         <div className="input-box">
